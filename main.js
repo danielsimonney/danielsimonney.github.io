9a8ebbb932ec646478e7e0890f3230a1cbc74894
daniel <daniel@MacBook-Pro-de-daniel.local>
2019-10-08 12:16:02 +0200

    rajout competence

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var menu=document.querySelector('.menu-toggler')
+var menu=document.querySelector('.menu-burger')
 var topNav=document.querySelector('.top-nav')
 menu.addEventListener("click", function(){
     menu.classList.toggle("open");
@@ -11,7 +11,17 @@ menu.addEventListener("click", function(){
     window.scrollTo(0, 0);
   });
 
+  
 AOS.init({
   easing:'ease',
   duration:'1800'
 })
+if(window.innerWidth<700){
+  table=(document.getElementsByTagName("div"));
+console.log(table)
+console.log(table.length)
+for (var i = 0; i < table.length; i++) {
+  document.getElementsByTagName("div")[i].removeAttribute("data-aos"); 
+}
+
+}
